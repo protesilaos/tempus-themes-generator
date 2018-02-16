ef1d136bdf4f9791646a0519ef711b4f21651748
Protesilaos Stavrou <info@protesilaos.com>
2018-02-16 11:07:08 +0200

    Exclude vi filetype definition from generator output

--- tempus-themes-generator.sh
+++ tempus-themes-generator.sh
@@ -59,7 +59,7 @@ trap 'rm -f $tempfile' 0 1 2 3 15 # Clean up the temporary file
 
 (
   echo 'cat <<END_OF_TEXT'
-  cat "$template_path"
+  cat "$template_path" | grep -v 'vi: ft='
   echo 'END_OF_TEXT'
 ) > $tempfile
 . $tempfile
